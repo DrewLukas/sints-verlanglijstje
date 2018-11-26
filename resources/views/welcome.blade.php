8b5983c1187d776cb6dc89250dee3a81a13d53ed
Jasper <j.drewes@telfort.nl>
2018-11-26 10:31:42 +0100

    Procfile aangemaakt / styling welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
             }
 
             .wrapper {
-                background-color: rgba(0,0,0, 0.2);
+                background-color: rgba(0,0,0, 0.3);
             }
 
             .full-height {
